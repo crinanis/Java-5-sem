3b562375d64b8f707e4c8fc09763b5cd4d29645d
ksesha <72476438+crinanis@users.noreply.github.com>
2022-12-01 11:50:02 +0300

    Delete Project/Contacts/Contacts directory

--- Project/Contacts/Contacts/src/main/java/com/example/Contacts/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.Contacts.repos;
-
-import com.example.Contacts.domain.ContactsUsers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<ContactsUsers, Long> {
-    ContactsUsers findByCuserLogin(String username);
-}
